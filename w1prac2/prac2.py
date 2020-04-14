38d586962cc8c3678b27fe789c02a6475d78bb0e
alex o <40974140+alxojy@users.noreply.github.com>
2020-04-14 12:26:10 +0800

    Upload z algo

---
+++ w1prac2/prac2.py
@@ -0,0 +1,90 @@
+'''
+def z_algorithm(str):
+    # initialise z array
+    z = [None]*len(str)
+    # initialise first position of z array
+    z[0] = len(str)
+    
+    L = 0
+    R = 0
+    for i in range(1,len(str)):
+        
+        # case 1 - no z box
+        if i > R:
+            count = 0
+            while (i + count) < z[0] and str[i+count] == str[count]:
+                count += 1
+            if count > 0:
+                L = i
+                R = i+count-1
+            z[i] = count
+        else:
+            remain = R-i+1
+            k = i-L
+
+            # case 2a 
+            if z[k] < remain:
+                z[i] = z[k]
+            
+            # case 2b
+            elif z[k] > remain:
+                z[i] = remain
+
+            # case 2c
+            else:
+                # compare str[remain] onwards
+                count = 0
+                while (i+remain+count) < z[0] and str[i+remain] == str[k]:
+                    count += 1
+                if count > 0:
+                    L = i
+                    R = i+remain-1
+                z[i] = remain+count-k
+
+    return z
+'''
+
+def z_algorithm(str):
+    n = len(str)
+    # initialise z array
+    z = [None]*n
+    # initialise first position of z array
+    z[0] = n
+    
+    L = 0
+    R = 0
+    for i in range(1,len(str)):
+        
+        # case 1 - no z box
+        if i > R:
+            count = 0
+            while (i + count) < z[0] and str[i+count] == str[count]:
+                count += 1
+            if count > 0:
+                L = i
+                R = i+count-1
+            z[i] = count
+        else:
+            remain = R-i+1
+            k = i-L
+
+            # case 2a 
+            if z[k] < remain:
+                z[i] = z[k]
+            
+            # case 2b
+            elif z[k] > remain:
+                z[i] = remain
+
+            # case 2c
+            else:
+                # compare str[remain] onwards
+                count = 0
+                while (i+remain+count) < z[0] and str[i+remain+count] == str[k-1+count]:
+                    count += 1
+                if count > 0:
+                    L = i
+                    R = i+remain+count-1
+                z[i] = remain+count
+
+    return z
