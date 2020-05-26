260838f1ab33c6b86063eec3cb01b6949496cc7f
alex o <40974140+alxojy@users.noreply.github.com>
2020-05-27 01:54:51 +0800

    miller rabin
    refactored

--- w8prac9/miller_rabin.py
+++ w8prac9/miller_rabin.py
@@ -2,25 +2,32 @@ import random
 import math
 
 def miller_rabin_randomized_primality(n, k):
-    # return true if it's composite
-    # return false if probably prime
-    if n % 2 == 0:
-        return "Composite1"
+    # return true if it's probably prime
+    # return false if composite
 
-    s, t = 0, n-1
+    if n == 2 or n == 3: # base cases
+        return True
+
+    if n % 2 == 0: # even number 
+        return False
+
+    s, t = 0, n-1 # initialisation 
     while t % 2 == 0:
         s += 1
         t = t >> 1 # divided by 2
-
+ 
     for _ in range(k):
-        a = random.randint(2,n-1)
+        a = random.randrange(2,n-1) # 2 to n-2 inclusive 
+
         if pow(a,n-1,n) != 1:
-            return "Composite2"
+            return False
         
         for i in range(1,s+1):
-            if pow(a, int(pow(2,i)*t), n) == 1 and pow(a, int(pow(2,(i-1))*t), n) != (1 or -1):
-                return "Probably prime"
-        
-    return "Probably prime"
+            
+            flag = True if pow(a, pow(2,i-1)*t, n) in (1,n-1) else False # check prev
 
-print(miller_rabin_randomized_primality(11,8))
+            if pow(a, pow(2,i)*t, n) == 1 and not flag:
+                return False
+        
+    return True # prime
+
